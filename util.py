84e97bb4f9a5725693c2423e3cd71a1515b94f32
Tianyu Lu <39132498+MauriceR71@users.noreply.github.com>
2019-09-19 22:20:41 -0400

    fixed type error

--- util.py
+++ util.py
@@ -313,7 +313,9 @@ def get_experimental_X_y_PET(random_state=1, train_size=150, return_test=False, 
     """Partition and add noise"""
     df = pd.read_csv('data/PETase_mutations_1_padded.csv')
     X,_ = get_gfp_X_y_aa(df, large_only=False, ignore_stops=True)
-    y_gt = np.log10(np.array(df["medianBrightness"]))
+    y_gt = np.array(df["medianBrightness"])
+    y_gt = y_gt.astype(np.float)
+    y_gt = np.log10(y_gt)
     max_cat = np.amax(y_gt)
     y_gt = np.multiply(y_gt, 1.0/max_cat)
     if return_test:
@@ -334,7 +336,9 @@ def get_experimental_X_y_Hydrolase(random_state=1, train_size=150, return_test=F
     """Partition and add noise"""
     df = pd.read_csv('data/hydrolase_padded.csv')
     X,_ = get_gfp_X_y_aa(df, large_only=False, ignore_stops=True)
-    y_gt = np.log10(np.array(df["medianBrightness"]))
+    y_gt = np.array(df["medianBrightness"])
+    y_gt = y_gt.astype(np.float)
+    y_gt = np.log10(y_gt)
     max_cat = np.amax(y_gt)
     y_gt = np.multiply(y_gt, 1.0/max_cat)
     if return_test:
